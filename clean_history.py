3114919828549cb7e86e10bf55a54bc7bd28788f
Maurits van Rees <maurits@vanrees.org>
2022-02-03 11:16:30 +0100

    black

--- clean_history.py
+++ clean_history.py
@@ -13,67 +13,82 @@ from Products.CMFCore.tests.base.security import OmnipotentUser
 from Products.CMFCore.tests.base.security import PermissiveSecurityPolicy
 from Products.CMFPlone.utils import base_hasattr
 
-version = '0.2.1'
+version = "0.2.1"
 usage = "Usage: /your/instance run clean_history.py [options] [sites]"
 description = (
     "Cleanup CMFEdition history in Plone sites. "
-    "Default is: all sites in the database.")
+    "Default is: all sites in the database."
+)
 p = optparse.OptionParser(
     usage=usage,
     version="%prog " + version,
     description=description,
-    prog="clean_history")
+    prog="clean_history",
+)
 p.add_option(
-    '--portal-type', '-p',
+    "--portal-type",
+    "-p",
     dest="portal_types",
     default=[],
     action="append",
     metavar="PORTAL_TYPE",
-    help=("Select to cleanup only histories for a kind of portal_type. "
-          "Default is all types. Can be called multiple times."))
+    help=(
+        "Select to cleanup only histories for a kind of portal_type. "
+        "Default is all types. Can be called multiple times."
+    ),
+)
 p.add_option(
-    '--keep-history', '-k',
+    "--keep-history",
+    "-k",
     type="int",
     dest="keep_history",
     default=None,
     metavar="HISTORY_SIZE",
-    help=('Before purging, temporary set the value of '
-          '"maximum number of versions to keep in the storage" to this '
-          'value in the portal_purgehistory. Default is: do not change '
-          'the value. In any case, the original value will be restored, '
-          'except when you use the --permanent option. '
-          'The minimum is 1: the original object is included '
-          'in the count.'))
+    help=(
+        "Before purging, temporary set the value of "
+        '"maximum number of versions to keep in the storage" to this '
+        "value in the portal_purgehistory. Default is: do not change "
+        "the value. In any case, the original value will be restored, "
+        "except when you use the --permanent option. "
+        "The minimum is 1: the original object is included "
+        "in the count."
+    ),
+)
 p.add_option(
-    '--permanent',
+    "--permanent",
     action="store_true",
     dest="permanent",
     metavar="PERMANENT",
-    help=('Set the "maximum number of versions to keep in the storage" in '
-          'the portal_purgehistory permanently to the --keep-history value. '
-          'Default is False: restore the original value. '
-          'This option is useful when you want to change the stored value '
-          'and purge all too old versions at the same time, '
-          'especially when the value was unlimited (-1) until now.'))
+    help=(
+        'Set the "maximum number of versions to keep in the storage" in '
+        "the portal_purgehistory permanently to the --keep-history value. "
+        "Default is False: restore the original value. "
+        "This option is useful when you want to change the stored value "
+        "and purge all too old versions at the same time, "
+        "especially when the value was unlimited (-1) until now."
+    ),
+)
 p.add_option(
-    '--dry-run',
+    "--dry-run",
     action="store_true",
     dest="dry_run",
     metavar="DRY_RUN",
-    help='Dry run: do not commit any changes.')
+    help="Dry run: do not commit any changes.",
+)
 p.add_option(
-    '--verbose',
-    '-v',
+    "--verbose",
+    "-v",
     action="store_true",
     default=False,
-    help="Show verbose output, for every cleaned content's history.")
+    help="Show verbose output, for every cleaned content's history.",
+)
 
 args = sys.argv[1:]
 # In Plone 4 and higher, the arguments we really want are prepended
 # with '-c' and '..../clean_history.py'.  Or possibly this depends on
 # the version of the plone.recipe.zope2instance recipe or the Zope
 # version.  So we strip this.
-if len(args) >= 2 and args[0] == '-c' and args[1].endswith('.py'):
+if len(args) >= 2 and args[0] == "-c" and args[1].endswith(".py"):
     args = args[2:]
 options, psite = p.parse_args(args)
 pp_type = options.portal_types
@@ -85,17 +100,18 @@ except NameError:
     sys.exit(1)
 
 if options.keep_history is not None and options.keep_history < 1:
-    print('Error: the keep history argument must be 1 or higher: '
-          'the original object is included in the count.')
+    print(
+        "Error: the keep history argument must be 1 or higher: "
+        "the original object is included in the count."
+    )
     sys.exit(1)
 
 
 def commit(note):
-    """Commit transaction, with note.
-    """
+    """Commit transaction, with note."""
     print(note)
     if options.dry_run:
-        print('Dry run selected, not committing.')
+        print("Dry run selected, not committing.")
         return
     # Commit transaction and add note.
     tr = transaction.get()
@@ -114,30 +130,41 @@ def spoofRequest(app):
     newSecurityManager(None, OmnipotentUser().__of__(app.acl_users))
     return makerequest(app)
 
+
 # Enable Faux HTTP request object
 app = spoofRequest(app)  # noqa
 
-sites = [(id, site) for (id, site) in app.items() if hasattr(
-    site, 'meta_type') and site.meta_type == 'Plone Site']
+sites = [
+    (id, site)
+    for (id, site) in app.items()
+    if hasattr(site, "meta_type") and site.meta_type == "Plone Site"
+]
 
-print('Starting analysis for %s. Types to cleanup: %s' % (
-    not psite and 'all sites' or ', '.join(psite),
-    not pp_type and 'all' or ', '.join(pp_type)))
+print(
+    "Starting analysis for %s. Types to cleanup: %s"
+    % (
+        not psite and "all sites" or ", ".join(psite),
+        not pp_type and "all" or ", ".join(pp_type),
+    )
+)
 for id, site in sites:
     if not psite or id in psite:
         print("Analyzing %s" % id)
         policy = site.portal_purgepolicy
         portal_repository = site.portal_repository
-        if (policy.maxNumberOfVersionsToKeep == -1 and
-                options.keep_history is None):
-            print("... maxNumberOfVersionsToKeep is -1 and no --keep-history "
-                  "argument has been given; skipping")
+        if policy.maxNumberOfVersionsToKeep == -1 and options.keep_history is None:
+            print(
+                "... maxNumberOfVersionsToKeep is -1 and no --keep-history "
+                "argument has been given; skipping"
+            )
             continue
 
         old_maxNumberOfVersionsToKeep = policy.maxNumberOfVersionsToKeep
         if options.keep_history is not None:
-            print("... Putting maxNumberOfVersionsToKeep from %d to %s" % (
-                old_maxNumberOfVersionsToKeep, options.keep_history))
+            print(
+                "... Putting maxNumberOfVersionsToKeep from %d to %s"
+                % (old_maxNumberOfVersionsToKeep, options.keep_history)
+            )
             if options.permanent:
                 print("This change is permanent.")
             else:
@@ -151,21 +178,20 @@ for id, site in sites:
         if pp_type:
             # Search without restrictions, like language.
             results = pc.unrestrictedSearchResults(portal_type=pp_type)
-        elif hasattr(pc, 'getAllBrains'):
+        elif hasattr(pc, "getAllBrains"):
             results = pc.getAllBrains()
         else:
             results = pc.unrestrictedSearchResults()
         for x in results:
             if options.verbose:
-                print("... cleaning history for %s (%s)" % (
-                    x.getPath(), x.portal_type))
+                print("... cleaning history for %s (%s)" % (x.getPath(), x.portal_type))
             try:
                 obj = x.getObject()
                 isVersionable = portal_repository.isVersionable(obj)
                 if isVersionable:
                     obj, history_id = dereference(obj)
                     policy.beforeSaveHook(history_id, obj)
-                    if base_hasattr(obj, 'version_id') and keep <= 1:
+                    if base_hasattr(obj, "version_id") and keep <= 1:
                         del obj.version_id
                     if options.verbose:
                         print("... cleaned!")
@@ -179,7 +205,7 @@ for id, site in sites:
 
         if not options.permanent:
             policy.maxNumberOfVersionsToKeep = old_maxNumberOfVersionsToKeep
-        commit('Purged CMFEditions history to %d versions.' % keep)
+        commit("Purged CMFEditions history to %d versions." % keep)
 
-print('End analysis')
+print("End analysis")
 sys.exit(0)
